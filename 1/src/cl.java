8cfcf3453adc83bdaa0850be4a04e679de6b457b
bethoven123 <aliya.hammatova@gmail.com>
2013-09-16 15:58:12 +0400

    Created new file

---
+++ 1/src/cl.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: student
+ * Date: 16.09.13
+ * Time: 15:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class cl {
+    int b = 0;
+}
